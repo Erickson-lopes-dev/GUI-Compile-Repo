a931890a135b40ba9d597f268448c4d15b6a280e
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-05-22 19:12:21 -0300

    centralizando gui e colocando uma barra de carregamento da gui

--- interface_main.py
+++ interface_main.py
@@ -1,39 +1,43 @@
-# from tkinter import *
-#
-# from main import CompileRepo
-# from scraping.scrapy_data_full import get_data_repository_full
-#
-# root = Tk()
-#
-# lista = Listbox(root, width=50, height=25)
-#
-# lista.pack()
-#
-# data = get_data_repository_full('Erickson-lopes-dev/Dica_Python_Linkedin')
-#
-# print(data)
-#
-# for file in data:
-#     if file['type_file'] == 'File' and file['extension'] != 'Go to parent directory':
-#         lista.insert(END, file['url'])
-#
-# root.mainloop()
-
 from tkinter import *
 from tkinter import ttk
-
+from tqdm import tqdm
 from scraping.scrapy_data_full import get_data_repository_full
 from scraping.scrapy_lines_bytes import get_lines_bytes
 
+data = get_data_repository_full('Erickson-lopes-dev/Universo_Programado_Numeros')
+
+
+class Centralizar:
+    def __init__(self, janela, largura, altura):
+        self.janela = janela
+        self.largura = largura
+        self.altura = altura
+
+        largura_sistema = janela.winfo_screenwidth()
+        altura_sistema = janela.winfo_screenheight()
+
+        self.posicao_x = int(largura_sistema / 2 - largura / 2)
+        self.posicao_y = int(altura_sistema / 2 - altura / 2)
+
+    def centralizado(self):
+        return self.janela.geometry('{}x{}+{}+{}'.format(self.largura,
+                                                         self.altura,
+                                                         self.posicao_x,
+                                                         self.posicao_y))
+
+
 ws = Tk()
 ws.title('PythonGuides')
-ws.geometry('400x300')
-ws['bg'] = '#fb0'
+# ws.geometry('600x600')
+# ws['bg'] = '#fb0'
 
-tv = ttk.Treeview(ws)
+janela_centralizada = Centralizar(ws, 600, 600)
+janela_centralizada.centralizado()
+
+tv = ttk.Treeview(ws, height=25)
 tv['columns'] = ('Name File', 'Extension', 'Lines', 'Size')
 tv.column('#0', width=0, stretch=NO)
-tv.column('Name File', anchor=CENTER, width=90)
+tv.column('Name File', anchor=CENTER, width=200)
 tv.column('Extension', anchor=CENTER, width=80)
 tv.column('Lines', anchor=CENTER, width=80)
 tv.column('Size', anchor=CENTER, width=80)
@@ -44,9 +48,7 @@ tv.heading('Extension', text='Extension', anchor=CENTER)
 tv.heading('Lines', text='Lines', anchor=CENTER)
 tv.heading('Size', text='Size', anchor=CENTER)
 
-data = get_data_repository_full('Erickson-lopes-dev/Compile-Repo')
-
-for num, file in enumerate(data):
+for num, file in enumerate(tqdm(data, desc='Construindo Tabela')):
     if file['type_file'] == 'File' and file['extension'] != 'Go to parent directory':
         lb = get_lines_bytes('https://github.com' + file['url'])
         # Adiciona uma linha na tabela
